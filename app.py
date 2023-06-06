7aeb345cfb2fcb1e1a370cb7d7b87bddf6ffc2df
github_username <first.last@doordash.com>
2023-06-06 14:36:03 -0700

    John -- use in-memory to store the previous chat history

--- app.py
+++ app.py
@@ -1,33 +1,29 @@
 import os
 import openai
-from flask import Flask, redirect, render_template, request, url_for,jsonify
+from flask import Flask, redirect, render_template, request, url_for,jsonify, g
 
 from general_prompt import build_general_insights_prompt, get_insights_json
 
 app = Flask(__name__)
+def initialize_map():
+    g.messages = [
+        {"role": "assistant", "content": "I am a helpful chatbot for Doordash"}
+    ]
+    g.initial_prompt = None
+
 openai.api_key = os.getenv("OPENAI_API_KEY")
 
+
+@app.before_request
+def before_request():
+    initialize_map()
 
 @app.route("/", methods=("GET", "POST"))
 def index():
     if request.method == "POST":
         input = request.form["user_ask"]
-        prompt = "Concise but explicit. " + input
-        # instead of providng raw data to open ai, we will call our internal services to build filtered analysis data
-        json_data = get_insights_json()
 
-        init_prompt = build_general_insights_prompt(json_data)
-
-        messages = [
-            {"role": "assistant", "content": "I am a helpful chatbot for Doordash"},
-            {"role": "system", "content": init_prompt},
-            {"role": "user", "content": prompt}
-        ]
-        response = openai.ChatCompletion.create(
-            model="gpt-3.5-turbo",
-            messages= messages
-        )
-        return redirect(url_for("index", result=response.choices[0].message.content))
+        return redirect(url_for("index", result=generate_reply(input)))
 
     result = request.args.get("result")
     return render_template("index.html", result=result)
@@ -35,9 +31,36 @@ def index():
 
 @app.route('/question', methods=['POST'])
 def process_questions():
-    questions = request.json.get('questions')
+    new_questions = request.json.get('questions')
     response = {
         'message': 'Received questions successfully',
-        'questions': questions
+        'questions': generate_reply(new_questions)
     }
     return jsonify(response), 200
+
+
+def generate_reply(input):
+    if g.initial_prompt is None:
+        json_data = get_insights_json()
+        g.initial_prompt = build_general_insights_prompt(json_data)
+        g.messages.append(
+            {"role": "system", "content": g.initial_prompt}
+        )
+
+    decorated_user_question = input + ". Try inspiring me with actions to take and prompt my next question"
+    g.messages.append(
+        {"role": "user", "content": decorated_user_question }
+    )
+
+    # instead of providing raw data to open ai, we will call our internal services to build filtered analysis data
+    response = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo",
+        messages=g.messages
+    )
+
+    result = response.choices[0].message.content
+
+    g.messages.append(
+        {"role": "assistant", "content": result}
+    )
+    return result
