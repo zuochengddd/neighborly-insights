73cf17541df6d0beb8ff03a1d226408573633804
zuochengddd <zuocheng.ding@doordash.com>
2023-06-06 13:37:54 -0700

    add recommendations

--- app.py
+++ app.py
@@ -6,8 +6,18 @@ from flask import Flask, redirect, render_template, request, url_for,jsonify
 
 
 general_prompt_tempalte ="""
-Here is the general information of our store:
+Here are the general information of our store:
 Our store name is {name} and located at {location}. And our sales data collected from Doordash like : {sales}. And our review summary collected by Doordash like : {review}
+
+When compare with other stores, we collect data from doordash, we honor following metrics based on priority scores. Here are metrics data for our store: {metrics}
+
+When makeing suggestions, please uses below data as priority:
+improve inflation : {inflation_rec}
+improve photo coverage : {photo_rec}
+improve menu variety: {menu_rec}
+improve promotion adoption : {promo_rec}
+improve ads adoption : {ads_rec}
+Below are some compatitors metrics data for references: {compatitors}
 """
 
 app = Flask(__name__)
@@ -18,15 +28,16 @@ openai.api_key = os.getenv("OPENAI_API_KEY")
 def index():
     if request.method == "POST":
         input = request.form["user_ask"]
-
+        prompt = "Concise but explicit. " + input
+        # instead of providng raw data to open ai, we will call our internal services to build filtered analysis data
         json_data = get_insights_json()
 
         init_prompt = build_general_insights_prompt(json_data)
 
         messages = [
             {"role": "assistant", "content": "I am a helpful chatbot for Doordash"},
-            {"role": "user", "content": init_prompt},
-            {"role": "user", "content": input}
+            {"role": "system", "content": init_prompt},
+            {"role": "user", "content": prompt}
         ]
         response = openai.ChatCompletion.create(
             model="gpt-3.5-turbo",
@@ -52,8 +63,25 @@ def build_general_insights_prompt(json_data):
     name = json_data["storeDetails"]["name"]
     sales_data = json_data["generalCompsAnalysis"]["sales"]
     review_data = json_data["generalCompsAnalysis"]["reviewComparison"]
-
-    return general_prompt_tempalte.format(name = name, location = location, sales = sales_data, review =review_data)
+    metrics = json_data["IndividualCompetitorAnalysis"]["metrics"]
+    compatitors = json_data["IndividualCompetitorAnalysis"]["comps"]
+    inflation_rec = json_data["IndividualCompetitorAnalysis"]["metrics"]["inflationRate"]["recommendedActions"]
+    photo_rec = json_data["IndividualCompetitorAnalysis"]["metrics"]["photoCoverage"]["recommendedActions"]
+    menu_rec = json_data["IndividualCompetitorAnalysis"]["metrics"]["menuVariety"]["recommendedActions"]
+    promo_rec = json_data["IndividualCompetitorAnalysis"]["metrics"]["promotionAdoption"]["recommendedActions"]
+    ads_rec = json_data["IndividualCompetitorAnalysis"]["metrics"]["AdsAdoption"]["recommendedActions"]
+    return general_prompt_tempalte.format(
+        name = name, 
+        location = location, 
+        sales = sales_data, 
+        review = review_data,
+        metrics = metrics,
+        inflation_rec = inflation_rec,
+        photo_rec = photo_rec,
+        menu_rec = menu_rec,
+        promo_rec = promo_rec,
+        ads_rec = ads_rec,
+        compatitors = compatitors)
 
 def get_insights_json():
     # dummy response from upstream dependencies
