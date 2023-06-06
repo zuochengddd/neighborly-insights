5e07373f0bf9e29d18acb6657ab70008690aed20
zuochengddd <zuocheng.ding@doordash.com>
2023-06-06 13:11:04 -0700

    general insight prompt

--- app.py
+++ app.py
@@ -1,7 +1,14 @@
 import os
 
 import openai
+import json
 from flask import Flask, redirect, render_template, request, url_for
+
+
+general_prompt_tempalte ="""
+Here is the general information of our store:
+Our store name is {name} and located at {location}. And our sales data collected from Doordash like : {sales}. And our review summary collected by Doordash like : {review}
+"""
 
 app = Flask(__name__)
 openai.api_key = os.getenv("OPENAI_API_KEY")
@@ -11,59 +18,37 @@ openai.api_key = os.getenv("OPENAI_API_KEY")
 def index():
     if request.method == "POST":
         input = request.form["user_ask"]
-        # print(input)
-        # prompt = generate_insights_prompt(input)
-        # print(prompt)
-        # response = openai.Completion.create(
-        #     model="text-davinci-003",
-        #     prompt=prompt,
-        #     temperature=0.6,
-        #     max_tokens=2000
-        # )
-        #print(response)
-        #response = build_optimzied_response()
-        response = build_chat_response()
+
+        json_data = get_insights_json()
+
+        init_prompt = build_general_insights_prompt(json_data)
+
+        messages = [
+            {"role": "assistant", "content": "I am a helpful chatbot for Doordash"},
+            {"role": "user", "content": init_prompt},
+            {"role": "user", "content": input}
+        ]
+        response = openai.ChatCompletion.create(
+            model="gpt-3.5-turbo",
+            messages= messages
+        )
         return redirect(url_for("index", result=response.choices[0].message.content))
 
     result = request.args.get("result")
     return render_template("index.html", result=result)
 
 
-def generate_prompt(animal):
-    return """Suggest three names for an animal that is a superhero.
+def build_general_insights_prompt(json_data):
+    location = json_data["storeDetails"]["location"]
+    name = json_data["storeDetails"]["name"]
+    sales_data = json_data["generalCompsAnalysis"]["sales"]
+    review_data = json_data["generalCompsAnalysis"]["reviewComparison"]
 
-Animal: Cat
-Names: Captain Sharpclaw, Agent Fluffball, The Incredible Feline
-Animal: Dog
-Names: Ruff the Protector, Wonder Canine, Sir Barks-a-Lot
-Animal: {}
-Names:""".format(
-        animal.capitalize()
-    )
+    return general_prompt_tempalte.format(name = name, location = location, sales = sales_data, review =review_data)
 
-def generate_insights_prompt(input):
-    return """
-    My store: Monthly Sales : $1000, average item price inflation rate: 1.2, and order rate 78/day.
-    Store 1: Monthly sales: $2000, average item price inflation rate: 1.0, order rate 200/day.
-    Store 2: Monthly sales: $3000, avereage item price inflation rate: 1.5 order rate  15/day
-
-    Question: {}, make sure it includes all the data attributes. """.format(input)
-
-
-
-# example respons: Your store keep me up to date by sending short sms about the product. You gain my trust because you make sure I'm buying the right product. However, you can improve by sending me vouchers discount to boost sales
-def build_optimzied_response():
-    return openai.Edit.create(
-        model="text-davinci-edit-001",
-        input="Your store did a good job but there are few more area can improve to boost sales",
-        instruction="Make it friendly and clear"
-    )
-
-
-def build_chat_response():
-    return openai.ChatCompletion.create(
-        model="gpt-3.5-turbo",
-        messages=[
-            {"role": "user", "content": "Hello!"}
-        ]
-    )
+def get_insights_json():
+    # dummy response from upstream dependencies
+    file_path = "utils/sampleUpstreamResponse.json" 
+    with open(file_path) as file:
+        data = json.load(file)
+        return data
